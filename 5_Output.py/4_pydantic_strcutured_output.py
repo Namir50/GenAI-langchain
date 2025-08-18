7e8d43dacc128dbfdb9163b7a5f7812daaed4a3a
Namir50 <namirsayyed50@gmail.com>
2025-08-18 15:05:07 +0530

    Pydantic conversion to dict and json

--- 5_Output.py/4_pydantic_strcutured_output.py
+++ 5_Output.py/4_pydantic_strcutured_output.py
@@ -7,10 +7,19 @@ class Student(BaseModel):
     age: Optional[int] = None #in pydantic we have to specify None if the value of attribute is not mentioned
     number : Optional[int] = None
     email: EmailStr  #pydantic has the ability to validate emails, throws the error if invalid
-    cgpi: float = Field(gt = 0, lt= 10.1) #field helps to set constraints or criterias
-    
+    cgpi: float = Field(gt = 0, lt= 10.1, description='overall pointers of all sem') #field helps to set constraints or criterias and can also used for descriptions
+    college: str = 'UCOE' #we can set default value as well
+     
 new_student = {'name':'namir','number':34, 'email':'namirsayyed50@gmail.com', 'cgpi' : 7.3}  #here if integer is passed as a string, pydantic automatically converts it into int 
 
 student = Student(**new_student)
 
 print(student)
+
+#we can also make dictionary out of it
+student_dict = dict(student)
+print(student_dict['name'])
+
+#we can also make json out of it
+student_json = student.model_dump_json()
+print(student_json)
