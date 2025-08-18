3a6b673c49b1345ac9cb5b54e775b8236007c716
Namir50 <namirsayyed50@gmail.com>
2025-08-18 15:27:10 +0530

    Json structured output

---
+++ 5_Output.py/6_json_strcutured_output.py
@@ -0,0 +1,11 @@
+#we use json when we are using multiple languages in one project
+{
+    'title':'student',
+    'description': 'schema about the students',
+    'type':'object',
+    'properties':{
+        'name':'string',
+        'age':'integer'
+    },
+    'required':['name']
+}
