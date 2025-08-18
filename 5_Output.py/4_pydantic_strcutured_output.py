fb181c0a23f6778a464a3639acd33c5f3ab28ba3
Namir50 <namirsayyed50@gmail.com>
2025-08-18 14:59:52 +0530

    Pydantic features

--- 5_Output.py/4_pydantic_strcutured_output.py
+++ 5_Output.py/4_pydantic_strcutured_output.py
@@ -1,11 +1,16 @@
 #pydantic helps in explaining what the data type should be when error is thrown
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 
 class Student(BaseModel):
     name: str
+    age: Optional[int] = None #in pydantic we have to specify None if the value of attribute is not mentioned
+    number : Optional[int] = None
+    email: EmailStr  #pydantic has the ability to validate emails, throws the error if invalid
+    cgpi: float = Field(gt = 0, lt= 10.1) #field helps to set constraints or criterias
     
-new_student = {'name':'namir'}
+new_student = {'name':'namir','number':34, 'email':'namirsayyed50@gmail.com', 'cgpi' : 7.3}  #here if integer is passed as a string, pydantic automatically converts it into int 
 
 student = Student(**new_student)
 
-print(student.name)
+print(student)
