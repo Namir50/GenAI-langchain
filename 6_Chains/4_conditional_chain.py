c8c194fab27e663699aaaad8121db72ace6c50ab
Namir50 <namirsayyed50@gmail.com>
2025-11-02 01:46:58 -0800

    conditional chain condition 2

--- 6_Chains/4_conditional_chain.py
+++ 6_Chains/4_conditional_chain.py
@@ -2,7 +2,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableParallel, RunnableBranch, RunnableLambda
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 from typing import Literal
@@ -24,8 +24,17 @@ prompt1 = PromptTemplate(
     partial_variables={'format_instruction':parser2.get_format_instructions()}
 )
 
+prompt2 = PromptTemplate(
+    template = 'Write an appropriate response to this postive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template = 'Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
 classifier_chain = prompt1 | model | parser2
 
 result = classifier_chain.invoke({'feedback':'This is a terrible smartphone'}).sentiment
 
-print(result)
