c0edd2b4ef799ab22aef89076550b90e2ebf9fdd
Namir50 <namirsayyed50@gmail.com>
2025-11-02 01:40:40 -0800

    output format using pydantic

--- 6_Chains/4_conditional_chain.py
+++ 6_Chains/4_conditional_chain.py
@@ -3,6 +3,9 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from langchain_core.runnables import RunnableParallel
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
 
 load_dotenv()
 
@@ -10,13 +13,19 @@ model = ChatGoogleGenerativeAI(model = 'gemini-2.5-pro')
 
 parser = StrOutputParser()
 
+class Feedback(BaseModel):
+    sentiment : Literal['positive','negative'] = Field(description='Give the sentiment of the following feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
 prompt1 = PromptTemplate(
-    template='Classify the sentiment of the following feedback text into positive or negative \n {feedback}',
-    input_variables=['feedback']
+    template='Classify the sentiment of the following feedback text into positive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
 )
 
-classifier_chain = prompt1 | model | parser
+classifier_chain = prompt1 | model | parser2
 
-result = classifier_chain.invoke({'feedback':'This is a terrible smartphone'})
+result = classifier_chain.invoke({'feedback':'This is a terrible smartphone'}).sentiment
 
 print(result)
